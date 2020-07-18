56b287481252d4269bf866483ca742f2b4a9885a
Shubham Kumar <63204440+Shubham7054@users.noreply.github.com>
2020-07-18 19:49:55 +0530

    .
    .

---
+++ DatInfo/main.js
@@ -0,0 +1,24 @@
+function getweather()
+{
+    var var1="";
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function()
+    {
+        if (this.readyState == 4 && this.status == 200)
+        {
+            var1 = JSON.parse(this.responseText);
+            document.getElementById('input1').value = var1.result.rank;
+            document.getElementById('input2').value = Number(var1.main.temp_min)-273;
+            document.getElementById('input3').value = Number(var1.main.temp_max)-273;
+            document.getElementById('input4').value = "100";
+            document.getElementById('input5').value = var1.wind.speed;
+            document.getElementById('input6').value = var1.wind.speed;
+        }
+        
+    };
+
+    var s=document.getElementById('username1').value;
+    xhttp.open("GET", "https://codeforces.com/api/user.rating?handle"+s, true);
+    xhttp.send();
+}

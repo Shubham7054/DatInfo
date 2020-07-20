c17db80a8fd035a13316a717106dceea1dc1357c
Shubham Kumar <63204440+Shubham7054@users.noreply.github.com>
2020-07-20 17:41:38 +0530

    Initial Commit

--- DatInfo/main.js
+++ DatInfo/main.js
@@ -8,16 +8,52 @@ function getData()
         if (this.readyState == 4 && this.status == 200)
         {
             var1 = JSON.parse(this.responseText);
-            document.getElementById('input1').value = var1.result.rank;
-            document.getElementById('input2').value = Number(var1.main.temp_min);
-            document.getElementById('input3').value = Number(var1.main.temp_max);
-            document.getElementById('input4').value = var1.result.rank;
-            document.getElementById('input5').value = '100';
-            document.getElementById('input6').value = var1.wind.speed;
+
+            var len=var1.result.length;
+            var mx=0;
+
+            for(let i=0;i<len;i++)
+            {
+                var x=var1.result[i].newRating;
+                if(x>mx)
+                mx=x;
+            }
+            var bestRank=100000;
+            for(let i=0;i<len;i++)
+            {
+                var x=var1.result[i].rank;
+                if(x<bestRank)
+                bestRank=x;
+            }
+            var hr =-100000;
+            var s="";
+
+            for(let i=1;i<len;i++)
+            {
+                var x=var1.result[i].newRating;
+                var y=var1.result[i].oldRating;
+                if(x-y>hr)
+                {
+                    hr=x-y;
+                    s=var1.result[i].contestName;
+                }
+            }
+
+
+            document.getElementById("testing").innerHTML = "Scroll Down To View Result";
+
+
+            document.getElementById('input1').value = var1.result[len-1].newRating;
+            document.getElementById('input2').value = mx;
+            document.getElementById('input3').value = bestRank;
+            document.getElementById('input4').value = len;
+            document.getElementById('input500').value = hr;
+            document.getElementById('input6').value = s;
         }
+        
     };
 
     var s=document.getElementById('username1').value;
-    xhttp.open("GET", "https://codeforces.com/api/user.rating?handle"+s, true);
+    xhttp.open("GET", "https://codeforces.com/api/user.rating?handle="+s, true);
     xhttp.send();
 }
